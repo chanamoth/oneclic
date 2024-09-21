4a82b2bc945b5d811921188a6793c4cad4a824f5
chanamoth <mario.rojas.chanamoth@gmail.com>
2024-09-21 00:08:34 -0500

    Splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'myApp',
-  webDir: 'www'
+  webDir: 'www',
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000, // Duración en milisegundos (3 segundos)
+      launchAutoHide: true,
+      backgroundColor: "#f4f5f8", // Color de fondo
+      androidScaleType: "CENTER_CROP", // Ajusta el logo en Android
+      showSpinner: true, // Si deseas mostrar un spinner
+      spinnerColor: "#232323" // Color del spinner
+    }
+  }
 };
 
 export default config;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { StatusBar, Style } from '@capacitor/status-bar';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 StatusBar.setBackgroundColor({ color: '#fff' });
 
@@ -9,5 +10,20 @@ StatusBar.setBackgroundColor({ color: '#fff' });
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    // Ocultar el splash screen de Capacitor después de que la app esté lista
+    SplashScreen.hide();
+
+    // Remover el splash screen personalizado
+    setTimeout(() => {
+      const splash = document.querySelector('.custom-splash-screen');
+      if (splash) {
+        splash.classList.add('hidden'); // Agregar una clase para ocultarlo con animación
+      }
+    }, 3000); // Tiempo en milisegundos antes de ocultar el splash personalizado
+  }
 }

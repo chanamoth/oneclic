4b055fcdcc3f6a5625f295617db60652020b5b2e
chanamoth <mario.rojas.chanamoth@gmail.com>
2024-09-21 23:17:16 -0500

    Estilos generales

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 import { LoginPage } from './login.page';
 import { LoginPageRoutingModule } from './login-routing.module';
-
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
@@ -19,4 +17,5 @@ import { SharedModule } from '../shared/shared.module';
   ],
   declarations: [LoginPage]
 })
+
 export class LoginPageModule { }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,18 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage {
+
+export class LoginPage implements OnInit {
+
+  @ViewChild(IonContent, { static: false }) content!: IonContent;
+
+  scrolled: boolean = false;
+
   email: string = '';  // Declaración de la propiedad 'email'
   password: string = '';  // Declaración de la propiedad 'password'
-  
+
   passwordType: string = 'password';
   passwordIcon: string = 'eye-off-outline';
 
   constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+  // Controla el scroll para mostrar o no la cabecera
+  onScroll(event: any) {
+    const scrollTop = event.detail.scrollTop;
+    this.scrolled = scrollTop > 50;
+  }
 
   togglePasswordVisibility() {
     this.passwordType = this.passwordType === 'password' ? 'text' : 'password';

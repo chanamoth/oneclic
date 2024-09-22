a788e214d48870b58ca6196794fa65eef4894624
chanamoth <mario.rojas.chanamoth@gmail.com>
2024-09-21 21:14:37 -0500

    Tiendas oficiales

--- src/app/oficial-stores/oficial-stores.page.ts
+++ src/app/oficial-stores/oficial-stores.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
 import { ApiService } from '../services/api.service';
 
 @Component({
@@ -9,17 +10,21 @@ import { ApiService } from '../services/api.service';
 
 export class OficialStoresPage implements OnInit {
 
+  @ViewChild(IonContent, { static: false }) content!: IonContent;
+
   scrolled: boolean = false;
-  loadedComercios: any[] = []; // Comercios cargados
-  filteredComercios: any[] = []; // Comercios filtrados para la búsqueda
+  comercios: any[] = []; // Todos los comercios disponibles (almacenados en memoria)
+  loadedComercios: any[] = []; // Comercios que se muestran progresivamente en la vista
   banners: any[] = []; // Almacena las imágenes del banner
-  comercios: any[] = []; // Todos los comercios disponibles
-  itemsPerPage = 10; // Cantidad por página
-  currentPage = 0; // Página actual
-  loadingMore = false; // Estado de carga
-  isLoading: boolean = true;
+  itemsPerPage = 10; // Comercios por página
+  currentPage = 1; // Página actual para el scroll infinito
+  loadingMore = false; // Estado para evitar múltiples solicitudes
+  isLoading: boolean = true; // Estado de carga inicial
   serverUrl: string;
-  showAlert: boolean = false; // Control de la alerta
+  showAlert: boolean = false; // Control de la alerta de "sin resultados"
+  searchTerm: string = ''; // Guarda el término de búsqueda actual
+  noMoreData = false; // Control para saber si ya no hay más datos para cargar
+  filteredComercios: any[] = []; // Comercios filtrados por búsqueda
 
   constructor(private apiService: ApiService) {
     this.serverUrl = this.apiService.getServerUrl();
@@ -29,13 +34,13 @@ export class OficialStoresPage implements OnInit {
     this.loadInitialData();
   }
 
-  // Método para manejar el evento de scroll
+  // Controla el scroll para mostrar o no la cabecera
   onScroll(event: any) {
     const scrollTop = event.detail.scrollTop;
     this.scrolled = scrollTop > 50;
   }
 
-  // Método para cargar los datos iniciales
+  // Cargar los datos iniciales de los comercios (solo una vez)
   loadInitialData() {
     this.apiService.post('search/get_rubros_app', {
       pais: 'PE',
@@ -45,75 +50,93 @@ export class OficialStoresPage implements OnInit {
     }).then(response => {
       response.subscribe((data: any) => {
         if (data && data.results && data.results.comercios) {
-          // Asignamos 'logoLoaded' a false inicialmente
+          // Almacenar todos los comercios que devuelve la API en 'comercios'
           this.comercios = data.results.comercios.comercios.map((comercio: any) => {
             return { ...comercio, logoLoaded: false };
           });
-          this.filteredComercios = this.comercios; // Mostrar todos los comercios inicialmente
+
+          // Resetear la lista cargada y cargar los primeros datos
+          this.resetComercios();
           this.banners = data.banners || [];
-          this.loadMore(); // Cargar los primeros 10 comercios
           this.isLoading = false;
         }
       });
     });
   }
 
-  // Método para cargar más comercios
+  // Resetear la lista de comercios cargados (para uso al filtrar o reiniciar)
+  resetComercios() {
+    this.currentPage = 1; // Reiniciar la página actual
+    this.loadedComercios = []; // Reiniciar los comercios cargados
+    this.noMoreData = false; // Habilitar la carga de más datos
+
+    // Cargar el primer bloque de datos según el término de búsqueda
+    this.applyFilter();
+  }
+
+  // Aplicar el filtro o cargar la lista completa progresivamente
+  applyFilter() {
+    let filteredComercios = this.comercios;
+
+    // Si hay un término de búsqueda, aplicar el filtro
+    if (this.searchTerm && this.searchTerm.trim() !== '') {
+      filteredComercios = this.getFilteredComercios();
+    }
+
+    // Mostrar los primeros 10 comercios de la lista filtrada o completa
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
+    const newItems = filteredComercios.slice(start, end);
+
+    // Añadir los nuevos comercios al bloque actual de loadedComercios
+    this.loadedComercios = [...this.loadedComercios, ...newItems];
+
+    // Si no se encontraron más elementos, marcar como no hay más datos
+    if (newItems.length < this.itemsPerPage) {
+      this.noMoreData = true;
+    }
+
+    this.currentPage++; // Incrementar la página para la siguiente carga
+  }
+
+  // Obtener los comercios filtrados por el término de búsqueda
+  getFilteredComercios() {
+    return this.comercios.filter((comercio: any) => {
+      return comercio.nombre.toLowerCase().includes(this.searchTerm);
+    });
+  }
+
+  // Método para cargar más comercios, ya sea filtrados o no
   loadMore(event?: any) {
-    if (this.loadingMore) return; // Evitar solicitudes múltiples
+    if (this.loadingMore || this.isLoading || this.noMoreData) {
+      if (event) event.target.complete();
+      return; // Evitar múltiples solicitudes
+    }
+
     this.loadingMore = true;
 
-    const start = this.currentPage * this.itemsPerPage;
-    const end = start + this.itemsPerPage;
-    const newItems = this.comercios.slice(start, end);
+    // Cargar más datos filtrados o completos según el estado actual
+    this.applyFilter();
 
-    this.loadedComercios = [...this.loadedComercios, ...newItems];
-    this.filteredComercios = this.loadedComercios; // Actualizar los comercios filtrados
-    this.currentPage++;
+    this.loadingMore = false;
 
     if (event) {
       event.target.complete();
     }
-
-    this.loadingMore = false;
-    if (newItems.length < this.itemsPerPage && event) {
-      event.target.disabled = true;
-    }
   }
 
-  // Método para manejar el input de búsqueda
+  // Manejar el input de búsqueda
   onSearch(event: any) {
-    const searchTerm = event.target.value.toLowerCase();
-    if (searchTerm && searchTerm.trim() !== '') {
-      this.filteredComercios = this.comercios.filter((comercio: any) => {
-        return comercio.nombre.toLowerCase().includes(searchTerm);
-      }).map((comercio: any) => {
-        // Restablecemos logoLoaded a false para cada comercio filtrado
-        return { ...comercio, logoLoaded: false };
-      });
-
-      // Si no se encuentran coincidencias, mostrar la alerta
-      if (this.filteredComercios.length === 0) {
-        this.showAlert = true;
-      } else {
-        this.showAlert = false; // Ocultar alerta si hay resultados
-      }
-
-    } else {
-      // Si no hay búsqueda, mostramos todos los comercios
-      this.filteredComercios = this.comercios.map((comercio: any) => {
-        return { ...comercio, logoLoaded: false };
-      });
-      this.showAlert = false; // Ocultar alerta si volvemos a la vista completa
-    }
+    this.searchTerm = event.target.value.toLowerCase(); // Guardar el término de búsqueda
+    this.resetComercios(); // Reiniciar la lista con el nuevo término de búsqueda
   }
 
-  // Método para refrescar los datos al usar el refresher
+  // Refrescar los datos de la página
   refreshData() {
     this.isLoading = true;
-    this.currentPage = 0; // Reiniciar la página actual
-    this.loadedComercios = []; // Vaciar los comercios cargados
-    this.filteredComercios = []; // Vaciar los comercios filtrados
+    this.currentPage = 1; // Reiniciar la página actual
+    this.noMoreData = false; // Habilitar la carga de más datos
+    this.loadedComercios = []; // Reiniciar los comercios cargados
     this.loadInitialData(); // Volver a cargar los datos
     setTimeout(() => {
       this.isLoading = false;

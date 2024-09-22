106704e049ac9404af6083ddc96ecca90be6eb57
chanamoth <mario.rojas.chanamoth@gmail.com>
2024-09-22 02:30:11 -0500

    Status bar f4f5f8

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,21 +2,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'myApp',
+  appName: 'OneClic',
   webDir: 'www',
   bundledWebRuntime: false,
   plugins: {
     SplashScreen: {
-      launchShowDuration: 3000,
+      launchShowDuration: 3000, // Duración en milisegundos (3 segundos)
       launchAutoHide: true,
-      backgroundColor: "#f4f5f8",
-      androidScaleType: "CENTER_CROP",
-      showSpinner: true,
-      spinnerColor: "#232323"
-    },
-    "StatusBar": {
-      "style": "LIGHT",
-      "backgroundColor": "#33000000"
+      backgroundColor: "#f4f5f8", // Color de fondo
+      androidScaleType: "CENTER_CROP", // Ajusta el logo en Android
+      showSpinner: true, // Si deseas mostrar un spinner
+      spinnerColor: "#232323" // Color del spinner
     }
   }
 };
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,30 @@
 import { Component } from '@angular/core';
-import { SplashScreen } from '@capacitor/splash-screen';
 import { StatusBar, Style } from '@capacitor/status-bar';
+import { SplashScreen } from '@capacitor/splash-screen';
+
+StatusBar.setStyle({ style: Style.Light });
+StatusBar.setBackgroundColor({ color: '#f4f5f8' });
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-
 export class AppComponent {
-
   constructor() {
     this.initializeApp();
   }
 
   initializeApp() {
-
+    // Ocultar el splash screen de Capacitor después de que la app esté lista
     SplashScreen.hide();
 
-    this.setupStatusBar();
-
+    // Remover el splash screen personalizado
     setTimeout(() => {
       const splash = document.querySelector('.custom-splash-screen');
       if (splash) {
-        splash.classList.add('hidden');
+        splash.classList.add('hidden'); // Agregar una clase para ocultarlo con animación
       }
-    }, 3000);
+    }, 3000); // Tiempo en milisegundos antes de ocultar el splash personalizado
   }
-
-  async setupStatusBar() {
-
-    await StatusBar.setOverlaysWebView({ overlay: true });
-    await StatusBar.setStyle({ style: Style.Light });
-  }
-
 }
